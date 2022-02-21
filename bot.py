31dc6f01a2cb63f031b3d90f77a999ae18d6fb7e
Zao0oza <sefeigin@mail.ru>
2022-02-21 23:32:04 +0300

    auto test add

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@ from asyncio import sleep
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.mongo import MongoStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from os import getenv
 import psycopg2
+from aiogram.types import BotCommand
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 # подгружаем токены и пароли из окружения
@@ -19,7 +21,8 @@ if not bot_token:
 # Объект бота
 bot = Bot(token=bot_token)
 # Диспетчер для бота
-dp = Dispatcher(bot, storage=MemoryStorage())
+storage = MongoStorage(host='localhost', port=27017, db_name='teaching_bot')
+dp = Dispatcher(bot, storage=storage)
 
 
 def sql_conn(sql_request, values: tuple = None):
@@ -54,38 +57,48 @@ class Form(StatesGroup):
     """
     Класс состояний для реализации конечных автоматов
     """
-    waiting_for_name = State()
-    waiting_for_answer = State()
+    name = State()
+    answer = State()
     finished = State()
-    waiting_check=State()
+    waiting_check = State()
 
 
-@dp.message_handler(commands="start")
-async def cmd_start(message: types.Message):
+@dp.message_handler(state='*', commands='cancel')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(state='*', commands="start")
+async def cmd_start(message: types.Message, state: FSMContext):
     """
     Комнада start проверяет по id есть ли ученик в БД
     Если есть, то предложит продолжить учебу,
     Нет, спросит имя ученика и перейдет к добавлению нового ученика в БД
     """
+
     sql_request = """SELECT PUPIL_NAME, EXERCISE, CHOICES, RIGHT_ANSWER 
                 FROM public.exercises
                 JOIN pupils on pupils.cur_exercise=exercises.exercise_id
                 WHERE pupil_id =%s """
     pupil_info = sql_conn(sql_request, (message.from_user.id,))
     if pupil_info:
-        await message.answer(f"С возвращением {pupil_info[0]}!", reply_markup=types.ReplyKeyboardRemove())
-        await Form.waiting_for_answer.set()
+        await Form.answer.set()
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add('Да')
-        await message.answer("Хотите продолжить учебу?", reply_markup=keyboard)
+        await message.answer(f"Здравствуйте {pupil_info[0]}!\n Готовы начать учится?", reply_markup=keyboard)
 
     else:
-        await Form.waiting_for_name.set()
+        await Form.name.set()
         await message.answer("Добро пожаловать! Как вас зовут?")
 
 
-@dp.message_handler(state=Form.waiting_for_answer)
-async def send_answer(message: types.CallbackQuery):
+@dp.message_handler(state=Form.answer)
+async def send_answer(message: types.CallbackQuery, state: FSMContext):
+    print(message, state)
     """
     Отправляет вопрос ученику
     """
@@ -94,16 +107,14 @@ async def send_answer(message: types.CallbackQuery):
                    JOIN pupils on pupils.cur_exercise=exercises.exercise_id
                    WHERE pupil_id =%s """
     pupil_info = sql_conn(sql_request, (message.from_user.id,))
-    keyboard = types.InlineKeyboardMarkup()
-    # задаем глобальные перемнные для уменьшения количества sql запросов
-    global name, right_answer, lesson, exercise_id
     name, question, choices, right_answer, exercise_id, lesson = [x for x in pupil_info[0:6]]
+    async with state.proxy() as data:
+        data['answer'] = {'name': name, 'right_answer': right_answer, 'lesson': lesson, 'exercise_id': exercise_id}
     choices += right_answer
     choices = choices.split(';')
-    await message.answer(question)
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*choices)
-    await message.answer(choices, reply_markup=keyboard)
+    await message.answer(str(question)+'\n'+str(choices), reply_markup=keyboard)
     await Form.waiting_check.set()
 
 
@@ -112,47 +123,48 @@ async def check_answer(message: types.Message, state: FSMContext):
     """
     Получает и проверяет ответ
     """
-    if message.text == right_answer:
-
-        await message.answer(f"Верно, Красава {name}!", reply_markup=types.ReplyKeyboardRemove())
-        sql_request = """SELECT MAX(EXERCISE_ID) FROM public.exercises
-                                        GROUP BY LESSON
-                                        HAVING LESSON = %s"""
-        last_exersize = sql_conn(sql_request, (lesson,))
-        if last_exersize[0] != exercise_id:
-            sql_request = """ UPDATE pupils 
-                                SET CUR_EXERCISE = CUR_EXERCISE + 1, ANSWERED = current_timestamp
-                                WHERE pupil_id = %s"""
-            sql_conn(sql_request, (message.from_user.id,))
-            await send_answer(message)
-            #await Form.waiting_for_answer.set()
-        else:
-            # получаем номер последнего урока и проверяем есть ли еще уроки
-            sql_request = """SELECT MAX(LESSON) FROM public.exercises"""
-            last_lesson = sql_conn(sql_request)
-            if last_lesson[0] != lesson:
+    async with state.proxy() as data:
+        if message.text == data['answer']['right_answer']:
+            await message.answer(f"Верно, Красава {data['answer']['name']}!", reply_markup=types.ReplyKeyboardRemove())
+            sql_request = """SELECT MAX(EXERCISE_ID) FROM public.exercises
+                                            GROUP BY LESSON
+                                            HAVING LESSON = %s"""
+            last_exersize = sql_conn(sql_request, (data['answer']['lesson'],))
+            if last_exersize[0] != data['answer']['exercise_id']:
                 sql_request = """ UPDATE pupils 
-                                                SET CUR_EXERCISE = CUR_EXERCISE + 1, ANSWERED = current_timestamp
-                                                WHERE pupil_id = %s"""
+                                    SET CUR_EXERCISE = CUR_EXERCISE + 1, ANSWERED = current_timestamp
+                                    WHERE pupil_id = %s"""
                 sql_conn(sql_request, (message.from_user.id,))
-                await Form.waiting_for_answer.set()
-                keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-                keyboard.add('Да')
-                await message.answer(f"Молодец! Урок {lesson} успешно пройден\n Готовы перейти к следующему уроку?",
-                                     reply_markup=keyboard)
+                await send_answer(message, state)
+
             else:
-                await state.finish()
-                keyboard = types.InlineKeyboardMarkup()
-                keyboard.add(types.InlineKeyboardButton(text="Да", callback_data="reset_user"))
-                await message.answer(
-                    "Поздравляю вы закончили наш курс, надеюсь вам понравилось!!!\nХотите сбросить прогресс и начать "
-                    "курс заново?",
-                    reply_markup=keyboard)
-    else:
-        await message.answer(f"Неверно, ну ты и дон-дон {name}!")
+                # получаем номер последнего урока и проверяем есть ли еще уроки
+                sql_request = """SELECT MAX(LESSON) FROM public.exercises"""
+                last_lesson = sql_conn(sql_request)
+                if last_lesson[0] != data['answer']['lesson']:
+                    sql_request = """ UPDATE pupils 
+                                                    SET CUR_EXERCISE = CUR_EXERCISE + 1, ANSWERED = current_timestamp
+                                                    WHERE pupil_id = %s"""
+                    sql_conn(sql_request, (message.from_user.id,))
+                    await Form.answer.set()
+                    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                    keyboard.add('Да')
+                    await message.answer(
+                        f"Молодец! Урок {data['answer']['lesson']} успешно пройден\n Готовы перейти к следующему уроку?",
+                        reply_markup=keyboard)
+                else:
+                    await Form.finished.set()
+                    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                    keyboard.add('Да')
+                    return await message.answer(
+                        '''Поздравляю вы успешно закончили наш курс, 
+                        надеюсь вам понравилось!!!
+                        Хотите сбросить прогресс и начать курс заново?''',
+                        reply_markup=keyboard)
+        else:
+            await message.answer(f"Неверно, ну ты и дон-дон {data['answer']['name']}!\n Повтори материал и попробуй еще раз")
 
-
-@dp.message_handler(state=Form.waiting_for_name)
+@dp.message_handler(state=Form.name)
 async def process_name(message: types.Message, state: FSMContext):
     """
     Создает нового ученика
@@ -160,25 +172,28 @@ async def process_name(message: types.Message, state: FSMContext):
     sql_request = """ INSERT INTO pupils (PUPIL_ID, PUPIL_NAME) VALUES (%s, %s)"""
     sql_conn(sql_request, (message.from_user.id, message.text,))
     await state.finish()
-    sql_request = """SELECT PUPIL_NAME, EXERCISE, CHOICES, RIGHT_ANSWER 
+    sql_request = """SELECT PUPIL_NAME 
                    FROM public.exercises
                    JOIN pupils on pupils.cur_exercise=exercises.exercise_id
                    WHERE pupil_id =%s """
     pupil_info = sql_conn(sql_request, (message.from_user.id,))
     await message.answer(f"Добро пожаловать {pupil_info[0]}!")
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(text="Да", callback_data="send_answer"))
+    await Form.answer.set()
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add('Да')
     await message.answer("Хотите начать учебу?", reply_markup=keyboard)
 
 
-@dp.callback_query_handler(state=Form.finished)
-@dp.callback_query_handler(text="reset_user")
-async def reset_user(call: types.CallbackQuery, state: FSMContext):
+@dp.message_handler(text="Да", state=Form.finished)
+async def reset_user(message: types.Message, state: FSMContext):
+    '''
+    Сбрасывает прогресс ученика
+    '''
     sql_request = """ UPDATE pupils 
                         SET CUR_EXERCISE = 1
                         WHERE pupil_id = %s"""
-    sql_conn(sql_request, (call.from_user.id,))
-    await call.message.answer("Нажмите /start чтобы начать учебу")
+    sql_conn(sql_request, (message.from_user.id,))
+    await cmd_start(message, state)
 
 
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -1,20 +1,117 @@
-import pytest
 from bot import *
-from unittest.mock import AsyncMock
+
+from pathlib import Path
+
+# This example uses the configuration of `config.ini` (see examples/README)
+examples_dir = Path(__file__).parent.parent.absolute()
+SESSION_NAME: str = "tgintegration_examples"
+
+import pytest
+from pyrogram import Client
+
+from tgintegration import BotController
+from tgintegration import Response
+
+api_id = 19442712
+api_hash = "fbf8febe2162a37c4bf2aeb2af309ee1"
+# This example uses the configuration of `config.ini` (see examples/README)
+examples_dir = Path(__file__).parent.parent.absolute()
+SESSION_NAME: str = "tgintegration_examples"
+
+
+# This example uses the configuration of `config.ini` (see examples/README)
+def create_client(session_name: str = SESSION_NAME) -> Client:
+    client = Client(
+        session_name=session_name,
+        workdir=examples_dir,
+        api_id=api_id, api_hash=api_hash
+    )
+    client.load_config()
+    return client
+
+
+@pytest.mark.asyncio
+async def test_quick_if_pupil_exist():
+    sql_request = """ UPDATE pupils 
+                           SET CUR_EXERCISE = 1
+                           WHERE pupil_id = 370647884"""
+    sql_conn(sql_request, ('',))
+    '''Прогоняем бота по всему курсу'''
+    controller = BotController(
+        peer="@rusteach_bot",  # We are going to run tests on https://t.me/BotListBot
+        client=create_client(),
+        max_wait=6,  # Maximum timeout for responses (optional)
+        wait_consecutive=4,  # Minimum time to wait for more/consecutive messages (optional)
+        raise_no_response=True,  # Raise `InvalidResponseError` when no response received (defaults to True)
+        global_action_delay=2.5,  # Choosing a rather high delay so we can follow along in realtime (optional)
+    )
+    sleep_time=0.5
+    async with controller.collect(count=1) as response:  # type: Response
+        await controller.send_command("/start")
+    await controller.send_command("да", add_bot_name=False)
+    await sleep(sleep_time)
+    for i in range(4):
+        await sleep(sleep_time)
+        await controller.send_command("right_answer", add_bot_name=False)
+    await sleep(sleep_time)
+    await controller.send_command("да", add_bot_name=False)
+    await sleep(sleep_time)
+    await controller.send_command("right_answer", add_bot_name=False)
+    await sleep(sleep_time)
+    await controller.send_command("да", add_bot_name=False)
+    await sleep(sleep_time)
+    async with controller.collect(count=2) as response:  # type: Response
+        await controller.send_command("right_answer", add_bot_name=False)
+    await controller.send_command("да", add_bot_name=False)
+    assert response.num_messages == 2
+    assert "молодец" in response.full_text.lower()
+
+
+@pytest.mark.skip
+@pytest.mark.asyncio
+async def test_complete_with_new_pupil():
+    '''Прогоняем бота по всему курсу'''
+    controller = BotController(
+        peer="@rusteach_bot",  # We are going to run tests on https://t.me/BotListBot
+        client=create_client(),
+        # max_wait=2,  # Maximum timeout for responses (optional)
+        wait_consecutive=1,  # Minimum time to wait for more/consecutive messages (optional)
+        raise_no_response=True,  # Raise `InvalidResponseError` when no response received (defaults to True)
+        # global_action_delay=2.5,  # Choosing a rather high delay so we can follow along in realtime (optional)
+    )
+    sql_request = """ DELETE FROM pupils WHERE pupil_id=370647884"""
+    sql_conn(sql_request, ('',))
+    async with controller.collect(count=1) as response:  # type: Response
+        await controller.send_command("/start")
+    assert response.num_messages == 1
+    await controller.send_command("test_pupil", add_bot_name=False)
+    async with controller.collect(count=1) as response:  # type: Response
+        await controller.send_command("да", add_bot_name=False)
+    assert response.num_messages == 1
+
+    async with controller.collect(count=1) as response:  # type: Response
+        await controller.send_command("wrong_answer", add_bot_name=False)
+    assert response.num_messages == 1
+    while 'поздравляю' not in response.full_text.lower():
+        async with controller.collect(count=2) as response:  # type: Response
+            await controller.send_command("right_answer", add_bot_name=False)
+        assert response.num_messages == 2
+        if "молодец" in response.full_text.lower():
+            await controller.send_command("да", add_bot_name=False)
+        else:
+            assert "верно" in response.full_text.lower()
 
 
 def test_get_pupil_from_db():
-    exercise = ('test_pupil','question','wrong answer;wrong answer;wrong answer;wrong answer;','right_answer')
+    exercise = ('test_pupil', 'question', 'wrong answer;wrong answer;wrong answer;wrong answer;', 'right_answer')
     assert exercise == sql_conn("""SELECT PUPIL_NAME, EXERCISE, CHOICES, RIGHT_ANSWER FROM public.exercises
                     JOIN pupils on pupils.cur_exercise=exercises.exercise_id
                     WHERE pupil_id =%s 
                     """, (1,))
 
+
 def test_last_answer_for_lesson_from_db():
-    exercise = ('test_pupil','question','wrong answer;wrong answer;wrong answer;wrong answer;','right_answer')
     assert (4,) == sql_conn("""SELECT MAX(EXERCISE_ID) FROM public.EXERCISEs
                                 GROUP BY LESSON
                                 HAVING LESSON  =%s 
                                 """, (1,))
-
-
